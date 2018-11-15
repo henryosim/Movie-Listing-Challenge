ac16d2164504cb88098b59795a909318af7a4ab8
Henry Osim <support@henryosim.com>
2018-11-15 13:58:11 +0000

    before push

--- src/app/showing-now/showing-now.component.ts
+++ src/app/showing-now/showing-now.component.ts
@@ -1,7 +1,7 @@
 import { log, isArray } from 'util';
 import { MovieService } from './../shared/movie.service';
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-showing-now',
@@ -16,7 +16,7 @@ export class ShowingNowComponent implements OnInit {
   public defaultRateFilter: number;
   public imagePath: string;
 
-  private loadedMovies: object[];
+  private loadedMovies: object[] = [];
   private loadedGenre: object[];
   private rateFilterMinVal = 0;
   private rateFilterMaxVal = 10;
@@ -27,14 +27,13 @@ export class ShowingNowComponent implements OnInit {
     this.imagePath = this.movieService.imageURL;
     this.defaultRateFilter = 3;
 
-
-    //loads array of movie objects once into loadedMovie object
+    /** loads array of movie objects once into loadedMovie object */
     this.movieService.GetNowPlaying().subscribe(res => {
       this.loadedMovies = res['results'];
       this.filterMovies();
     });
 
-    //loads array of genre objects once into loadedGenre object
+    /** loads array of genre objects once into loadedGenre object */
     this.movieService.GetGenres().subscribe(res => {
 
       this.loadedGenre = res['genres'];
@@ -49,13 +48,19 @@ export class ShowingNowComponent implements OnInit {
       this.filterMovies();
     });
 
+    // create array of rating values from 0-10 with increment of -.5
     for (let index = this.rateFilterMinVal; index <= this.rateFilterMaxVal; index += 0.5) {
       this.rateFilterOptions.push(index);
     }
+
+
   }
 
   ngOnInit() { }
 
+  /**
+   * Filters the movie results
+   */
   filterMovies() {
     this.movies = [];
     this.setselectedGenreIDs();
@@ -67,20 +72,30 @@ export class ShowingNowComponent implements OnInit {
         if (movie['vote_average'] >= this.defaultRateFilter && this.selectedGenreIDs.indexOf(genreID) >= 0) {
           const genreName = this.getGenreName(genreID);
           movie['genreNames'].push(genreName);
-          this.movies.push(movie);
-
-          console.log(this.selectedGenreIDs);
-          console.log(genreID);
-          // console.log(movie['genreNames']);
         }
 
       });
+
+      this.movies.push(movie); // adds movies to the Movie array.
+
     });
 
-    console.log(this.movies);
-    console.log(this.defaultRateFilter);
+
+    this.movies.sort(this.compare); // sort movies that will be displayed
   }
 
+  /** sorting function,  to sort movies by popularity */
+  compare(b, a) {
+    if (a.popularity < b.popularity) {
+      return -1;
+    }
+    if (a.popularity > b.popularity) {
+      return 1;
+      return 0;
+    }
+  }
+
+  /** takes in genre id and outputs the genre name */
   getGenreName(id: number): string {
     let name = '';
     this.genres.forEach(genre => {
@@ -91,6 +106,7 @@ export class ShowingNowComponent implements OnInit {
     return name;
   }
 
+  /** iterates through all genres and retrieves all selected genre filters */
   setselectedGenreIDs() {
     this.selectedGenreIDs = [];
     this.genres.forEach(genre => {
